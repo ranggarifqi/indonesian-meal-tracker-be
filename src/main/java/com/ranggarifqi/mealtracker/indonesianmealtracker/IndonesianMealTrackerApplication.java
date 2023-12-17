5fcdbbde13cb7a759dad4c7c10a4ca21154eb0c2
Rangga Rifqi <rangga@ranggarifqi.com>
2023-12-17 11:36:38 +0700

    First commit

---
+++ src/main/java/com/ranggarifqi/mealtracker/indonesianmealtracker/IndonesianMealTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ranggarifqi.mealtracker.indonesianmealtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndonesianMealTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndonesianMealTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranggarifqi/mealtracker/indonesianmealtracker/IndonesianMealTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranggarifqi.mealtracker.indonesianmealtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndonesianMealTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
