2b13cd9c02666b3664d73049bba799f6eeb26738
Rangga Rifqi <rangga@ranggarifqi.com>
2023-12-23 13:00:44 +0700

    Added Nutrition Record

---
+++ src/main/java/com/ranggarifqi/mealtracker/indonesianmealtracker/models/records/Nutrition.java
@@ -0,0 +1,15 @@
+package com.ranggarifqi.mealtracker.indonesianmealtracker.models.records;
+
+public record Nutrition(
+  double calories,
+  double totalFat,
+  double saturatedFat,
+  double cholesterol,
+  double sodium,
+  double totalCarbohydrate,
+  double dietaryFiber,
+  double sugars,
+  double protein,
+  double potassium,
+  double phosphorus
+) {}
