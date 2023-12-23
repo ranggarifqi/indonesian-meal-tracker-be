a6c6be9484827947e5c29b2c0a9853ba2016f5b8
Rangga Rifqi <rangga@ranggarifqi.com>
2023-12-23 19:08:14 +0700

    Added IRecipeRepository.findByName

--- src/main/java/com/ranggarifqi/mealtracker/indonesianmealtracker/repositories/IRecipeRepository.java
+++ src/main/java/com/ranggarifqi/mealtracker/indonesianmealtracker/repositories/IRecipeRepository.java
@@ -3,5 +3,6 @@ package com.ranggarifqi.mealtracker.indonesianmealtracker.repositories;
 import com.ranggarifqi.mealtracker.indonesianmealtracker.models.Recipe;
 
 public interface IRecipeRepository {
+  Recipe findByName(String searchQuery);
   void create(Recipe payload);
 }
