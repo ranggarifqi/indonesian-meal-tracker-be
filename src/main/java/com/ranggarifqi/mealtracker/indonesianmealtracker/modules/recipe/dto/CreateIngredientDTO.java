7be1687f6372aefb5bc70e3a6cc3d2f1a7c0f5d6
Rangga Rifqi <rangga@ranggarifqi.com>
2023-12-24 07:12:34 +0700

    Added boilerplate to CreateIngredientDTO

--- src/main/java/com/ranggarifqi/mealtracker/indonesianmealtracker/modules/recipe/dto/CreateIngredientDTO.java
+++ src/main/java/com/ranggarifqi/mealtracker/indonesianmealtracker/modules/recipe/dto/CreateIngredientDTO.java
@@ -22,4 +22,61 @@ public class CreateIngredientDTO {
   private Nutrition nutrition;
 
   private IngredientMetadata metadata;
+
+  public CreateIngredientDTO(String name, float servingQty, String servingUnit, double servingWeightInGram, Nutrition nutrition, IngredientMetadata metadata) {
+    this.name = name;
+    this.servingQty = servingQty;
+    this.servingUnit = servingUnit;
+    this.servingWeightInGram = servingWeightInGram;
+    this.nutrition = nutrition;
+    this.metadata = metadata;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public float getServingQty() {
+    return servingQty;
+  }
+
+  public void setServingQty(float servingQty) {
+    this.servingQty = servingQty;
+  }
+
+  public String getServingUnit() {
+    return servingUnit;
+  }
+
+  public void setServingUnit(String servingUnit) {
+    this.servingUnit = servingUnit;
+  }
+
+  public double getServingWeightInGram() {
+    return servingWeightInGram;
+  }
+
+  public void setServingWeightInGram(double servingWeightInGram) {
+    this.servingWeightInGram = servingWeightInGram;
+  }
+
+  public Nutrition getNutrition() {
+    return nutrition;
+  }
+
+  public void setNutrition(Nutrition nutrition) {
+    this.nutrition = nutrition;
+  }
+
+  public IngredientMetadata getMetadata() {
+    return metadata;
+  }
+
+  public void setMetadata(IngredientMetadata metadata) {
+    this.metadata = metadata;
+  }
 }
