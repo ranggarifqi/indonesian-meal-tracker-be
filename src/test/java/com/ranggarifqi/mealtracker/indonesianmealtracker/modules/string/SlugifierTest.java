8d55564aed5f1edd1b2023cf1d79b9549d2fc107
Rangga Rifqi <rangga@ranggarifqi.com>
2023-12-24 11:06:45 +0700

    Removed unnecessary anno

--- src/test/java/com/ranggarifqi/mealtracker/indonesianmealtracker/modules/string/SlugifierTest.java
+++ src/test/java/com/ranggarifqi/mealtracker/indonesianmealtracker/modules/string/SlugifierTest.java
@@ -2,10 +2,7 @@ package com.ranggarifqi.mealtracker.indonesianmealtracker.modules.string;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 public class SlugifierTest {
 
   @Test
